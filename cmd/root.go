ca8daf50a79cef17574697d60e58c26fd610f74e
Marius Scurtescu <marius.scurtescu@gmail.com>
2022-12-29 17:12:59 -0800

    set root comand short and long descriptions

--- cmd/root.go
+++ cmd/root.go
@@ -34,13 +34,9 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "oauth-commander",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "OAuth 2 and OpenID Connect command line client",
+	Long: `A command line client that allows you to explore and interact with both
+OAuth 2 and OpenID Connect Authorization servers.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
