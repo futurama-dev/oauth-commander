582d4a08dda4cebdb1c56dd48d09dc2bf2672872
Sebastian Scurtescu <sebastiansc.scurtescu@gmail.com>
2022-12-31 02:06:50 -0800

    added type flag, and switch case for the different types

--- cmd/server_discover.go
+++ cmd/server_discover.go
@@ -24,6 +24,7 @@ package cmd
 import (
 	"fmt"
 	"github.com/futurama-dev/oauth-commander/discovery"
+	"github.com/futurama-dev/oauth-commander/oauth2"
 	"github.com/futurama-dev/oauth-commander/oidc"
 	"github.com/spf13/cobra"
 	"log"
@@ -42,29 +43,28 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		issuer, err := cmd.Flags().GetString("issuer")
+		layerType, err := cmd.Flags().GetString("type")
 
 		if err != nil {
 			log.Fatalln(err)
 		}
 
-		discoveryUrl, err := oidc.BuildDiscoveryUrl(issuer)
-
-		if err != nil {
-			log.Fatalln(err)
+		switch layerType {
+		case "all":
+			fmt.Println("trying all types")
+			fmt.Println("OIDC")
+			fmt.Println(oidcDiscovery(issuer))
+			fmt.Println("OAuth2")
+			fmt.Println(oauth2Discovery(issuer))
+		case "oidc":
+			fmt.Println("trying oidc")
+			fmt.Println(oidcDiscovery(issuer))
+		case "oauth2":
+			fmt.Println("trying oauth 2")
+			fmt.Println(oauth2Discovery(issuer))
+		default:
+			fmt.Println("invalid type inputted: ", layerType)
 		}
-
-		oidcConfig, err := discovery.FetchDiscovery(discoveryUrl)
-
-		if err == discovery.NotFoundErr {
-			fmt.Println("OpenID Connect discovery not found!")
-			os.Exit(0)
-		}
-
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		fmt.Println(oidcConfig)
 	},
 }
 
@@ -72,4 +72,47 @@ func init() {
 	serverCmd.AddCommand(discoverCmd)
 
 	discoverCmd.Flags().StringP("issuer", "i", "", "Authorization server issuer to run discovery against.")
+	discoverCmd.Flags().StringP("type", "t", "all", "Which top layer to use on top of OAuth 2")
+}
+
+func oidcDiscovery(issuer string) string {
+	discoveryUrl, err := oidc.BuildDiscoveryUrl(issuer)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	oidcConfig, err := discovery.FetchDiscovery(discoveryUrl)
+
+	if err == discovery.NotFoundErr {
+		fmt.Println("OpenID Connect discovery not found!")
+		os.Exit(0)
+	}
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return oidcConfig
+}
+
+func oauth2Discovery(issuer string) string {
+	discoveryUrl, err := oauth2.BuildDiscoveryUrl(issuer)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	oauth2Config, err := discovery.FetchDiscovery(discoveryUrl)
+
+	if err == discovery.NotFoundErr {
+		fmt.Println("OAuth 2 Connect discovery not found!")
+		os.Exit(0)
+	}
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return oauth2Config
 }
